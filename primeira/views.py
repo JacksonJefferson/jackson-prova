9c84f443b41421ea8f3f384675859ccc15a95aa6
Aluno <aluno@ifpi.edu.br>
2019-09-12 19:04:01 -0300

    preparacao de templates

--- primeira/views.py
+++ primeira/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+#from . models import Paciente
+#from . forms import PacienteForm
 
-# Create your views here.
+def home(request):
+    return render (request, 'home.html')

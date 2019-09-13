cdc4e627f260f16f558d30c14f37bec552101560
JacksonJefferson <jackson.jefferson389@gmail.com>
2019-09-13 18:32:38 -0300

    Criação de relacionamentos

--- primeira/models.py
+++ primeira/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Cliente(models.Model):
+    nome = models.CharField(max_length=50)
+    idade = models.IntegerField()
+    sexo = models.CharField(max_length=1)
+    cpf = models.CharField(max_length=11)
+
+class Produto(models.Model):
+    nome = models.CharField(max_length=50)
+    quantidade = models.IntegerField()
+    descricao = models.CharField(max_length=100)
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+
+class CarrinhoDeCompras(models.Model):
+    cor = models.CharField(max_length=50)
+    tamanho = models.CharField(max_length=1)
+    produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
